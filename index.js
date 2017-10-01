65850d85f36b6a7641bdf3b54ed16e5d6db102b0
Asma Sadia <a.sadia51299@gmail.com>
2017-10-01 20:29:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ function logWhisper(string){
     console.log('hello')
 }
 function sayHiToGrandma(string){
-  var string = 'hello'
-
- return string.toLowerCase() === string
-
-
-
+  if (string.toLowerCase() === string) {
+       return "I can't hear you!"
+     }
+   
+     if (string.toUpperCase() === string) {
+      return "YES INDEED!"
+     }
+   
+     if (string === "I love you, Grandma.") {
+      return "I love you, too."
 }
